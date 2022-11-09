2d7e71e45c936bdbba0292efe0a40a12066cb707
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-11-09 14:43:34 +0700

    Research script workflow agar bisa berjalan di github workflow

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -36,7 +36,6 @@ abstract class DuskTestCase extends BaseTestCase
         ])->unless($this->hasHeadlessDisabled(), function ($items) {
             return $items->merge([
                 '--disable-gpu',
-                '--no-sandbox',
                 '--headless'
             ]);
         })->all());
